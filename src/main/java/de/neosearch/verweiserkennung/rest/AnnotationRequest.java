3bc08249e15ec5258fc263c3ad2c5e78671d7fba
daniel <d.danielbeck@googlemail.com>
2018-07-14 10:00:56 +0200

    Added RestController

---
+++ src/main/java/de/neosearch/verweiserkennung/rest/AnnotationRequest.java
@@ -0,0 +1,22 @@
+package de.neosearch.verweiserkennung.rest;
+
+public class AnnotationRequest {
+	private String text;
+	private String targetPlatform; // urteile-gesetze (default) oder opendata
+
+	public String getText() {
+		return text;
+	}
+
+	public String getTargetPlatform() {
+		return targetPlatform;
+	}
+
+	public void setTargetPlatform(String targetPlatform) {
+		this.targetPlatform = targetPlatform;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
